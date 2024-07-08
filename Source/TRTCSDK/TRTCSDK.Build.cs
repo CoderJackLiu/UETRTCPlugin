5d19509bc910359a6b97bb906fb1f6c0ac07688a
Liuqi <596770421@qq.com>
2024-07-08 18:55:03 +0800

    删除其他平台支持

--- Source/TRTCSDK/TRTCSDK.Build.cs
+++ Source/TRTCSDK/TRTCSDK.Build.cs
@@ -22,47 +22,14 @@ public class TRTCSDK : ModuleRules
 			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Win64", "liteav.dll"));
 			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Win64", "txffmpeg.dll"));
 			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Win64", "txsoundtouch.dll"));
+			PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Win64", "liteav_screen.dll"));
 
 			RuntimeDependencies.Add("$(BinaryOutputDir)/liteav.dll", Path.Combine(ModuleDirectory, "Win64", "liteav.dll"));
 			RuntimeDependencies.Add("$(BinaryOutputDir)/txffmpeg.dll", Path.Combine(ModuleDirectory, "Win64", "txffmpeg.dll"));
 			RuntimeDependencies.Add("$(BinaryOutputDir)/txsoundtouch.dll", Path.Combine(ModuleDirectory, "Win64", "txsoundtouch.dll"));
+			RuntimeDependencies.Add("$(BinaryOutputDir)/txsoundtouch.dll", Path.Combine(ModuleDirectory, "Win64", "liteav_screen.dll"));
 			
 		}
-		else if (Target.Platform == UnrealTargetPlatform.Mac)
-		{
-			PublicFrameworks.Add(Path.Combine(ModuleDirectory, "Mac", "TXLiteAVSDK_TRTC_Mac.framework"));
-			PublicFrameworks.Add(Path.Combine(ModuleDirectory, "Mac", "TXFFmpeg.framework"));
-			PublicFrameworks.Add(Path.Combine(ModuleDirectory, "Mac", "TXSoundTouch.framework"));
-		}
-		else if (Target.Platform == UnrealTargetPlatform.IOS)
-		{
-			PublicAdditionalFrameworks.Add(new Framework("TXLiteAVSDK_TRTC", Path.Combine(ModuleDirectory, "iOS", "TXLiteAVSDK_TRTC.framework.zip"), "", true));
-			PublicAdditionalFrameworks.Add(new Framework("TXFFmpeg", Path.Combine(ModuleDirectory, "iOS", "TXFFmpeg.framework.zip"), "", true));
-			PublicAdditionalFrameworks.Add(new Framework("TXSoundTouch", Path.Combine(ModuleDirectory, "iOS", "TXSoundTouch.framework.zip"), "", true));
-
-			PublicFrameworks.AddRange(
-						new string[] {
-							"CoreServices",
-							"VideoToolbox",
-							"AudioToolbox",
-							"Accelerate",
-							"CFNetwork",
-							"OpenGLES",
-							"MetalKit",
-							"CoreTelephony"
-				}
-			);
-		}
-		else if (Target.Platform == UnrealTargetPlatform.Android)
-		{
-			AdditionalPropertiesForReceipt.Add("AndroidPlugin", Path.Combine(ModuleDirectory, "Android", "UPL_TRTCSDK.xml"));
-
-			// string Architecture = "armeabi-v7a";
-			string Architecture = "arm64-v8a";
-
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", Architecture, "libliteavsdk.so"));
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", Architecture, "libtxsoundtouch.so"));
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "Android", Architecture, "libtxffmpeg.so"));
-		}
+	
 	}
 }
